cd5016ded90e533ec3dcb636781cd05ee4138742
Tommy Truong <imtommytr@gmail.com>
2026-03-31 15:20:01 -0500

    Ensure server listens on all network interfaces

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use((err, req, res, next) => {
 });
 
 //      START SERVER 
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
     console.log(`Server running at https://cosc3380-13429-team1-personal.onrender.com`);
 });
 
